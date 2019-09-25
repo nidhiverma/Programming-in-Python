d219af669e8103ff8805884b51ef2f4a3acd95c8
Nidhi Verma <nidhiverma1997@gmail.com>
2019-09-26 02:13:47 +0530

    added documentation

--- factorial.py
+++ factorial.py
@@ -1,4 +1,10 @@
 def factorial(n):
+     '''
+    Objective: find factorial of a number
+    Input Parameters:
+        n: an integer
+    Return Values: factorial of n
+    '''
     if n == 1 or n == 0:
         return 1
     return n * factorial(n-1)
