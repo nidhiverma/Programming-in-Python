bc137f8b9d43ed47d1aa403f6a68c1303764ad06
Nidhi Verma <nidhiverma1997@gmail.com>
2019-09-25 23:36:52 +0530

    Create gcd.py
    function to find gcd of two numbers

---
+++ gcd.py
@@ -0,0 +1,11 @@
+def gcd(a, b):
+    '''
+    Objective: find gcd of 2 numbers
+    Input Parameters:
+        a: an integer
+        b: an integer
+    Return Values: gcd of a & b
+    '''
+    if b == 0:
+        return a
+    return gcd(b, a % b)
