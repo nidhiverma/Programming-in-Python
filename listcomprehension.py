5e566fdadc5d79b6e0594c65fd2dafe4f0960175
Sunidhi Jain <33275429+SunidhiJain@users.noreply.github.com>
2019-10-04 16:59:15 +0530

    Create listcomprehension.py

---
+++ listcomprehension.py
@@ -0,0 +1,15 @@
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+
+    output = ''
+    print('[', end='')
+    for i in range(x+1):
+        for j in range(y+1):
+            for k in range(z+1):
+                if i+j+k != n:
+                    output += '[{0}, {1}, {2}], '.format(i,j,k)
+    output = output.rstrip(' ,')+']'
+    print(output)
